e25f75712bd805d7b5128965c0659fbde40c8e7b
szymon <szymon.popielarz@gmail.com>
2021-06-28 16:52:00 +0200

    changed place for redux provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import Root from './views/Root';
 import reportWebVitals from './reportWebVitals';
+import { reducers } from './reducers';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 reportWebVitals();
--- client/src/views/Root.tsx
+++ client/src/views/Root.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import { ThemeProvider } from 'styled-components';
 import { routes } from '../routes/index';
 import MainTemplate from '../templates/MainTemplate';
 import AddPostTemplate from '../templates/AddPostTemplate';
 import AuthTemplate from '../templates/AuthTemplate';
 import SearchResultTemplate from '../templates/SearchResultTemplate';
-import { reducers } from '../reducers';
 import { theme } from '../theme/mainTheme';
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 function Root() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path={routes.home} component={MainTemplate} />
-            <Route exact path={routes.addPost} component={AddPostTemplate} />
-            <Route exact path={routes.auth} component={AuthTemplate} />
-            <Route exact path={routes.search} component={SearchResultTemplate} />
-          </Switch>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={routes.home} component={MainTemplate} />
+          <Route exact path={routes.addPost} component={AddPostTemplate} />
+          <Route exact path={routes.auth} component={AuthTemplate} />
+          <Route exact path={routes.search} component={SearchResultTemplate} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
